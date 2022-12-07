163f461ab6b814832a611e6c22392a4cb03701fb
Alexis Salas <orbitamcnetwork@gmail.com>
2022-12-06 22:09:39 -0600

    Beta2.0

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
- <head>
-        <meta name="description"content="BITACORA  RESIDENCIAS ITSPR">
-        <meta name="key"content="BITACORA, REDICENCIAS, ITSPR, INSTITUTO TECNOLOGICO SUPERIOR DE POZA RICA">
-        <META NAME="AUTHOR"CONTENT="Omar Nayef Pineda Blanco">
-        <title>BITACORA ITSPR</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="shortcut icon" href="IMAGENES_BD/LOGO_ITSPR.jpg" type="image/x-icon">
-        <img  src="IMAGENES_BD/Tira_logo.jpg"width="1400" height="200" >
+
+<head>
+    <meta name="description" content="BITACORA  RESIDENCIAS ITSPR">
+    <meta name="key" content="BITACORA, REDICENCIAS, ITSPR, INSTITUTO TECNOLOGICO SUPERIOR DE POZA RICA">
+    <META NAME="AUTHOR" CONTENT="Omar Nayef Pineda Blanco">
+    <title>BITACORA ITSPR</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="IMAGENES_BD/LOGO_ITSPR.jpg" type="image/x-icon">
+    <img src="IMAGENES_BD/Tira_logo.jpg" width="1400" height="200">
 </head>
 
 <body>
-         
-         <header>
-            <nav class="nav-supe">
-                <a href="index.php" class="input-nav-supe">Inicio</a>
-                <a href="registro.php" class="input-nav-supe">REGISTRARSE</a>  
-                <a href="login.php" class="input-nav-supe">INICIAR SESION</a>
-                <a href="archivos.html" class="input-nav-supe">ARCHIVOS</a>
-               
-                <label for="Tipo_Documento">DOCUMENTACION</label>        
+
+    <header>
+        <nav class="nav-supe">
+            <a href="index.php" class="input-nav-supe">Inicio</a>
+            <a href="registro.php" class="input-nav-supe">REGISTRARSE</a>
+            <a href="login.php" class="input-nav-supe">INICIAR SESION</a>
+
+            <label for="Tipo_Documento">DOCUMENTACION</label>
             <select name="Tipo_Documento" id="Tipo_Documento">
                 <option value="SOLICITUD RESIDENCIA" href="SOLICITUD RESIDENCIAS.html">1:SOLICITUD RESIDENCIA</option>
                 <option value="CARTA ACEPTACION">2:CARTA ACEPTACION</option>
@@ -30,19 +30,22 @@
                 <option value="EVALUCION Y REPORTE">7:EVALUCION Y REPORTE</option>
                 <option value="CARTA TERMINACION RESIDENCIAS">8:CARTA TERMINACION RESIDENCIAS</option>
                 <option value="LIBERACION DE REPORTE RESIDENCIAS">9:LIBERACION DE REPORTE RESIDENCIAS</option>
-            </select></nav>
-        
-            <div>
+            </select>
+        </nav>
+
+        <div>
             <section class="textos-header">
                 <h1><span>BITACORA DE RESIDENCIAS</span></h1>
                 <h2><span>FACILITANDO LA VIDA ESTUDIANTIL</span></h2>
-            </section></div>
-            
+            </section>
+        </div>
 
-         </header>
-         <div aling="center"><img src="IMAGENES_BD/paisaje 2.jpg" width="700" height="480" ></div>
 
-         
-        
+    </header>
+    <div aling="center"><img src="IMAGENES_BD/paisaje 2.jpg" width="700" height="480"></div>
+
+
+
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,41 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta><script src="https://kit.fontawesome.com/ba719609d3.js" crossorigin="anonymous"></script>
-        <meta name="description"content="BITACORA  RESIDENCIAS ITSPR">
-        <meta name="key"content="BITACORA, REDICENCIAS, ITSPR, INSTITUTO TECNOLOGICO SUPERIOR DE POZA RICA">
-        <META NAME="AUTHOR"CONTENT="Omar Nayef Pineda Blanco">
-        <title>BITACORA ITSPR</title>
-        <link rel="stylesheet" href="style.css">
-        <link rel="shortcut icon" href="IMAGENES_BD/LOGO_ITSPR.jpg" type="image/x-icon">
-        <img  src="IMAGENES_BD/Tira_logo.jpg"width="1400" height="200" >
-    </head>
 
-    <body>
-        <header>
-            <nav>
-                <a href="index.php">Inicio</a>
-                <a href="registro.php" target="_blank">REGISTRARSE</a>  
-                <a href="login.php"target="_blank">INICIAR SESION</a>
-            </nav>
-        </header>
-        <form class="LOGIN" method="post" action="login_logica.php"></form>
+<head>
+    <meta>
+    <script src="https://kit.fontawesome.com/ba719609d3.js" crossorigin="anonymous"></script>
+    <meta name="description" content="BITACORA  RESIDENCIAS ITSPR">
+    <meta name="key" content="BITACORA, REDICENCIAS, ITSPR, INSTITUTO TECNOLOGICO SUPERIOR DE POZA RICA">
+    <META NAME="AUTHOR" CONTENT="Omar Nayef Pineda Blanco">
+    <title>BITACORA ITSPR</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="IMAGENES_BD/LOGO_ITSPR.jpg" type="image/x-icon">
+    <img src="IMAGENES_BD/Tira_logo.jpg" width="1400" height="200">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <a href="index.php">Inicio</a>
+            <a href="registro.php" target="_blank">REGISTRARSE</a>
+            <a href="login.php" target="_blank">INICIAR SESION</a>
+        </nav>
+    </header>
+    <form class="LOGIN" method="post" action="Logica/login_logica.php">
         <H1>INICIA SESION</H1>
         <Div class="contenedor1">
 
             <div class="input-contenedor1">
                 <i class="fa-solid fa-person icon"></i>
-                <input type="text" name="num_control" placeholder="NUMERO DE CONTROL">
+                <input type="text" name="num_control" placeholder="NUMERO DE CONTROL" required>
             </div>
 
             <div class="input-contenedor1">
                 <i class="fa-solid fa-person icon"></i>
-                <input type="password" name="contraseña" placeholder="CONTRASEÑA">
+                <input type="password" name="contraseña" placeholder="CONTRASEÑA" required>
             </div>
 
-            <input type="submit" value="Logear" >
+            <input type="submit" value="Logear">
         </Div>
-        </form>
-    </body>
+    </form>
+</body>
 
 </html>
